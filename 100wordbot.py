269039b3cf481c49c602939a96633c026eab216e
Chivalrybean <44534661+Chivalrybean@users.noreply.github.com>
2021-06-05 09:56:46 -0700

    Initial files

---
+++ 100wordbot.py
@@ -0,0 +1,20 @@
+import discord
+import requests as r
+from discord.ext import commands
+from discord_slash import SlashCommand, SlashContext
+
+from tokens import token, guild_ids
+
+client = commands.Bot(command_prefix="/")
+slash = SlashCommand(client, sync_commands=True)
+
+
+@client.event
+async def on_ready():
+    print('The bot has logged in as {0.user}'.format(client))
+
+@slash.slash(name="100word", description="Get a random 100 word story by Laurence Simon")
+async def _100word(ctx: SlashContext):
+   await ctx.send(r.get("https://oneadayuntilthedayidie.com/?page_id=26987").content.decode("utf-8"))
+
+client.run(token)
